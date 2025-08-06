Refactor and fix descriptor computation

src/tracking.py
@@ -8,39 +8,17 @@ import plot
 from image import Image
 from localization import ransacLocalization
 from structure_from_motion import sfm
+from utils.utils_cv2 import from_cv2, to_cv2
 
 # TODO: add a note about notation / documentation regarding (x,y) vs (y,x)
 
 
-def to_cv2(p_P: np.ndarray) -> np.ndarray:
-    """
-    Convert format of points to match what cv2 expects
-
-    Args:
-        p_P (np.ndarray): (2xN)
-
-    Returns:
-        np.ndarray: (Nx1x2)
-    """
-    p_P = p_P.astype(np.float32)
-    N = p_P.shape[1]
-    return p_P.T.reshape((N, 1, 2))
-
-
-def from_cv2(p_P: np.ndarray) -> np.ndarray:
-    """
-    Convert format of points from cv2 format to (2xN)
-
-    Args:
-        p_P (np.ndarray): (Nx1x2)
-
-    Returns:
-        np.ndarray: (2xN)
-    """
-    return p_P.T.reshape(2, -1)
-
-
-def run_klt(images: Sequence[Image], p_P_keypoints_initial: np.ndarray, K: np.ndarray):
+def run_klt(
+    images: Sequence[Image],
+    p_P_keypoints_initial: np.ndarray,
+    p_W_landmarks_initial: np.ndarray,
+    K: np.ndarray,
+):
     """
     Run KLT on the images
 
@@ -55,6 +33,8 @@ def run_klt(images: Sequence[Image], p_P_keypoints_initial: np.ndarray, K: np.nd
         maxLevel=2,
         criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
     )
+    print(p_P_keypoints_initial.shape)
+    print(p_W_landmarks_initial.shape)
 
     p0_P_keypoints_cv2 = to_cv2(p_P_keypoints_initial)
     for i_0, i_1 in zip(images, images[1:]):
@@ -64,16 +44,16 @@ def run_klt(images: Sequence[Image], p_P_keypoints_initial: np.ndarray, K: np.nd
         )
 
         # Select good points
-        p0_P_keypoints_cv2 = p0_P_keypoints_cv2[st == 1]
-        p1_P_keypoints_cv2 = p1_P_keypoints_cv2[st == 1]
+        # p0_P_keypoints_cv2 = p0_P_keypoints_cv2[st == 1]
+        # p1_P_keypoints_cv2 = p1_P_keypoints_cv2[st == 1]
 
-        p_W, _, _ = sfm.run_sfm(
-            p1_P=from_cv2(p0_P_keypoints_cv2), p2_P=from_cv2(p1_P_keypoints_cv2), K=K
-        )
+        # p_W, _, _ = sfm.run_sfm(
+        #     p1_P=from_cv2(p0_P_keypoints_cv2), p2_P=from_cv2(p1_P_keypoints_cv2), K=K
+        # )
         p0_P_keypoints = from_cv2(p0_P_keypoints_cv2)
         R_C_W, t_C_W, best_inlier_mask, _, _ = ransacLocalization(
             p_P_keypoints=p0_P_keypoints,
-            p_W_landmarks=p_W,
+            p_W_landmarks=p_W_landmarks_initial,
             K=K,
         )
         plot.plot_keypoints(img=i_0.img, p_P_keypoints=p0_P_keypoints)
@@ -89,4 +69,5 @@ def run_klt(images: Sequence[Image], p_P_keypoints_initial: np.ndarray, K: np.nd
         )
 
         # p0_P_keypoints_cv2 = p1_P_keypoints_cv2.reshape(-1, 1, 2)
+        p_W_landmarks_initial = p_W_landmarks_initial[:, best_inlier_mask]
         p0_P_keypoints_cv2 = to_cv2(from_cv2(p1_P_keypoints_cv2)[:, best_inlier_mask])