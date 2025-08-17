2eeebd998438bed812e6871e62a86fb0c321cd8d
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-08-17 18:05:05 +0200

    Add new tests world to camera

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -21,3 +21,57 @@ def test_world_to_camera_identity_transformation():
         ]
     )
     assert np.array_equal(world_to_camera(p_W_hom, T_C_W), p_W_hom[:3, :])
+
+
+def test_world_to_camera_translation():
+    p_W_hom = np.array(
+        [
+            [1, 9, 2],
+            [3, 0, 3],
+            [7, 1, 0],
+            [1, 1, 1],
+        ]
+    )
+    T_C_W = np.array(
+        [
+            [1, 0, 0, 3],
+            [0, 1, 0, 2],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1],
+        ]
+    )
+    p_W_expected = np.array(
+        [
+            [4, 12, 5],
+            [5, 2, 5],
+            [8, 2, 1],
+        ]
+    )
+    assert np.array_equal(world_to_camera(p_W_hom, T_C_W), p_W_expected)
+
+
+def test_world_to_camera_scaling():
+    p_W_hom = np.array(
+        [
+            [1, 9, 2],
+            [3, 0, 3],
+            [7, 1, 0],
+            [1, 1, 1],
+        ]
+    )
+    T_C_W = np.array(
+        [
+            [1, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 0, 3, 0],
+            [0, 0, 0, 1],
+        ]
+    )
+    p_W_expected = np.array(
+        [
+            [1, 9, 2],
+            [6, 0, 6],
+            [21, 3, 0],
+        ]
+    )
+    assert np.array_equal(world_to_camera(p_W_hom, T_C_W), p_W_expected)
