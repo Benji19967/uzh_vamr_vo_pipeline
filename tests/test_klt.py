3dc07797b36dba9b048f99df5fe29e8cd7ee20c4
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-08-19 18:37:55 +0200

    Modify test klt

--- tests/test_klt.py
+++ tests/test_klt.py
@@ -13,19 +13,19 @@ def test_klt():
 
     p0_I_keypoints = np.array(
         [
-            [50, 20],
-            [50, 20],
+            [50, 20, 87],
+            [50, 20, 87],
         ]
     )
     p1_I_keypoints_expected = np.array(
         [
-            [52, 21],
-            [52, 21],
+            [52, 21, -999],
+            [52, 21, -999],
         ]
     )
 
     p1_I_keypoints, status = run_klt(image_0, image_1, p0_I_keypoints)
 
-    assert np.allclose(p1_I_keypoints, p1_I_keypoints_expected, atol=1)
-    assert status.shape == (2,)
-    assert np.allclose(status, np.array([True, True], dtype=bool))
+    assert np.allclose(p1_I_keypoints[:, :2], p1_I_keypoints_expected[:, :2], atol=1e-1)
+    assert status.shape == (3,)
+    assert np.allclose(status, np.array([True, True, False], dtype=bool))
